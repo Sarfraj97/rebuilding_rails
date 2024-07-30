7157c69578056d16edae9a9065db2ef48b66bd3e
akhilesh patel <akhilesh.patel@axel.org>
2024-07-30 22:56:22 +0530

    adding routes

--- lib/r001z.rb
+++ lib/r001z.rb
@@ -5,11 +5,27 @@ require_relative "r001z/version"
 module R001z
   class App
     def call(env)
+      kl, act = cont_and_act(env)
+      text = kl.new(env).send(act)
       [
         200, 
         {'Content-type' => 'text/html'},
-        ["hello web !!!"]
+        [text]
       ]
+    end
+
+    def cont_and_act(env)
+      _, con, act, after = env["PATH_INFO"].split("/")
+      con = con.capitalize + "Controller"
+      [Object.const_get(con), act] 
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
   
